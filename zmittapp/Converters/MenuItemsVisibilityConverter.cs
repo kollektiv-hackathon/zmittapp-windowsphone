d810ef81a7ce827e9d93dbfbfcf6e097dccfafde
Lorenz Wolf <lorenz.wolf@bluewin.ch>
2014-12-27 15:22:52 +0100

    design, default value for menues

---
+++ zmittapp/Converters/MenuItemsVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using zmittapp.DataModel;
+
+namespace zmittapp.Converters
+{
+   public class MenuItemsVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return value;
+
+            ObservableCollection<MenuItem> menus = value as ObservableCollection<MenuItem>;
+
+            return (menus.Count > 0) ? Visibility.Visible : Visibility.Collapsed; 
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
